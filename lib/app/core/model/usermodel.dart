aa64bec475551f102571c0713076afca3fc90731
Davee <johndaveaquino35@gmail.com>
2024-09-16 15:01:20 +0800

    Update usermodel.dart

--- lib/app/core/model/usermodel.dart
+++ lib/app/core/model/usermodel.dart
@@ -4,13 +4,19 @@ class UserModel {
   final String? id;
   final String email;
   final String password;
+  final String mobileNo;
 
-  UserModel({this.id, required this.email, required this.password});
+  UserModel(
+      {this.id,
+      required this.email,
+      required this.password,
+      required this.mobileNo});
 
   toJson() {
     return {
       "email": email,
       "password": password,
+      "mobileNumber": mobileNo,
     };
   }
 
@@ -18,6 +24,9 @@ class UserModel {
       DocumentSnapshot<Map<String, dynamic>> document) {
     final data = document.data()!;
     return UserModel(
-        id: document.id, email: data['email'], password: data['password']);
+        id: document.id,
+        email: data['email'],
+        password: data['password'],
+        mobileNo: data['mobileNumber']);
   }
 }
